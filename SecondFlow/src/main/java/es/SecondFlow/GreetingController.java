af9e1b6b2812900dc02f11fdf03b4e0a73f97ed8
SergioA <123589171+SergioAlvSch@users.noreply.github.com>
2023-02-09 15:00:16 +0100

    Cambios html y clase producto + controladores

--- SecondFlow/src/main/java/es/SecondFlow/GreetingController.java
+++ SecondFlow/src/main/java/es/SecondFlow/GreetingController.java
@@ -6,10 +6,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class GreetingController {
- @GetMapping("/greeting")
+ @GetMapping("/")
  public String greeting(Model model) {
- model.addAttribute("name", "World");
- return "greeting_template";
+  model.addAttribute("name", "World");
+  return "greeting_template";
+ }
+ @GetMapping("/Formulario")
+ public String Formulario(Model model) {
+  model.addAttribute("name", "World");
+  return "Formulario";
  }
 }
 
---
+++ SecondFlow/src/main/java/es/SecondFlow/Producto.java
@@ -0,0 +1,69 @@
+package es.SecondFlow;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Producto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String nombre;
+    private String categoria;
+    private String descripcion;
+
+    protected Producto() {
+    }
+
+    public Producto(Long id, String nombre, String categoria, String descripcion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.categoria = categoria;
+        this.descripcion = descripcion;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", categoria='" + categoria + '\'' +
+                ", descripcion='" + descripcion + '\'' +
+                '}';
+    }
+}
