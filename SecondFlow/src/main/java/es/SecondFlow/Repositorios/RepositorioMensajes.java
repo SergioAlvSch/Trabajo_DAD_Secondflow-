f8b2320d3fdf3a217037c3da1a0d4702fbb71269
SergioA <123589171+SergioAlvSch@users.noreply.github.com>
2023-02-26 21:02:43 +0100

    Update and rename SecondFlow/src/main/java/es/SecondFlow/RepositorioMensajes.java to SecondFlow/src/main/java/es/SecondFlow/Repositorios/RepositorioMensajes.java

--- SecondFlow/src/main/java/es/SecondFlow/Repositorios/RepositorioMensajes.java
+++ SecondFlow/src/main/java/es/SecondFlow/Repositorios/RepositorioMensajes.java
@@ -1,5 +1,6 @@
-package es.SecondFlow;
+package es.SecondFlow.Repositorios;
 
+import es.SecondFlow.Entidades.Mensaje;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RepositorioMensajes extends JpaRepository<Mensaje, Long> {
