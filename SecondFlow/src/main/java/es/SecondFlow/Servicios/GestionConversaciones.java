ef3374ec55ff4c94547d2444d360e88799b621b6
EduardVasileStancu <123593621+EduardVasileStancu@users.noreply.github.com>
2023-02-26 21:06:49 +0100

    Update and rename SecondFlow/src/main/java/es/SecondFlow/GestionConversaciones.java to SecondFlow/src/main/java/es/SecondFlow/Servicios/GestionConversaciones.java
    modificación de métodos y mover el archivo a su carpeta correspondiente

--- SecondFlow/src/main/java/es/SecondFlow/Servicios/GestionConversaciones.java
+++ SecondFlow/src/main/java/es/SecondFlow/Servicios/GestionConversaciones.java
@@ -1,8 +1,13 @@
-package es.SecondFlow;
+package es.SecondFlow.Servicios;
 
+import es.SecondFlow.Entidades.Conversacion;
+import es.SecondFlow.Entidades.Producto;
+import es.SecondFlow.Repositorios.RepositorioConversaciones;
+import es.SecondFlow.Entidades.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,7 +21,7 @@ public class GestionConversaciones {
         return repositorio.findById(id);
     }
 
-    public Conversacion findByUsuarios(Usuario usuario1,Usuario usuario2,Producto producto) {
+    public Conversacion findByUsuarios(Usuario usuario1, Usuario usuario2, Producto producto) {
         for (Conversacion aux: repositorio.findAll()){
             if ((aux.getEmisor().equals(usuario1)&&aux.getReceptor().equals(usuario2)&&aux.getProducto()==producto)){
                 return aux;
@@ -25,11 +30,17 @@ public class GestionConversaciones {
         return null;
     }
 
-    public void update(){repositorio.flush();}
-
-    public boolean exist(long id) {
-        return repositorio.existsById(id);
+    public List<Conversacion> findByProducto(Producto producto) {
+        List<Conversacion> auxConversaciones=new ArrayList<>();
+        for (Conversacion aux: repositorio.findAll()){
+            if ((aux.getProducto()==producto)){
+                auxConversaciones.add(aux);
+            }
+        }
+        return auxConversaciones;
     }
+
+    public void update(){repositorio.flush();}
 
     public List<Conversacion> findAll() {
         return repositorio.findAll();
