7101173ceb09a87f7b23def0ccc71ccf7d3e82d8
SergioA <123589171+SergioAlvSch@users.noreply.github.com>
2023-02-26 21:04:28 +0100

    Update and rename SecondFlow/src/main/java/es/SecondFlow/RepositorioProductos.java to SecondFlow/src/main/java/es/SecondFlow/Repositorios/RepositorioProductos.java

--- SecondFlow/src/main/java/es/SecondFlow/Repositorios/RepositorioProductos.java
+++ SecondFlow/src/main/java/es/SecondFlow/Repositorios/RepositorioProductos.java
@@ -1,6 +1,6 @@
-package es.SecondFlow;
+package es.SecondFlow.Repositorios;
+import es.SecondFlow.Entidades.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RepositorioProductos extends JpaRepository<Producto, Long> {
-
 }
