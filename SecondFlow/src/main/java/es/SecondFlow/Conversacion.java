e6b400bb6d671212089c4b84ec0a30fc088d4276
EduardVasileStancu <123593621+EduardVasileStancu@users.noreply.github.com>
2023-02-26 15:21:35 +0100

    Update Conversacion.java
    se modifican algunas variables y métodos para que se ajusten mejor al resto de las clases

--- SecondFlow/src/main/java/es/SecondFlow/Conversacion.java
+++ SecondFlow/src/main/java/es/SecondFlow/Conversacion.java
@@ -13,23 +13,44 @@ public class Conversacion {
     private Long id;
 
     @ManyToOne
+    Producto producto;
+
+    @ManyToOne
     Usuario emisor;
 
     @ManyToOne
     Usuario receptor;
 
-    @OneToMany(mappedBy = "conversacionEmisor", cascade = CascadeType.ALL, orphanRemoval = true)
-    List<Mensaje> conversacionEmisor;
-
-    @OneToMany(mappedBy = "conversacionReceptor", cascade = CascadeType.ALL, orphanRemoval = true)
-    List<Mensaje> conversacionReceptor;
-
-    public void setId(Long id) {
-        this.id = id;
-    }
+    @OneToMany(mappedBy = "conversacion", cascade = CascadeType.ALL, orphanRemoval = true)
+    List<Mensaje> listaMensajes;
 
     public Long getId() {
         return id;
     }
 
+    public Usuario getEmisor() {
+        return emisor;
+    }
+
+    public Usuario getReceptor() {
+        return receptor;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public List<Mensaje> getListaMensajes() {
+        return listaMensajes;
+    }
+
+    public Conversacion(Usuario emisor, Usuario receptor,Producto producto) {
+        super();
+        this.emisor = emisor;
+        this.receptor = receptor;
+        this.producto= producto;
+    }
+    public Conversacion() {
+
+    }
 }
