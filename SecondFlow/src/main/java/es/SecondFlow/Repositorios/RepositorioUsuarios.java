cce4d94337f595d3f60c6305abcd8f6da6ed3cdd
SergioA <123589171+SergioAlvSch@users.noreply.github.com>
2023-02-26 21:05:19 +0100

    Update and rename SecondFlow/src/main/java/es/SecondFlow/RepositorioUsuarios.java to SecondFlow/src/main/java/es/SecondFlow/Repositorios/RepositorioUsuarios.java

--- SecondFlow/src/main/java/es/SecondFlow/Repositorios/RepositorioUsuarios.java
+++ SecondFlow/src/main/java/es/SecondFlow/Repositorios/RepositorioUsuarios.java
@@ -1,5 +1,6 @@
-package es.SecondFlow;
+package es.SecondFlow.Repositorios;
 
+import es.SecondFlow.Entidades.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RepositorioUsuarios extends JpaRepository<Usuario, Long> {
