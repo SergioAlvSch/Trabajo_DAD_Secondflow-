92190f2778f7f8f577ca561fbe807147f03d72b9
SergioA <123589171+SergioAlvSch@users.noreply.github.com>
2023-02-26 16:57:07 +0100

    Update Usuario.java
    Quitamos métodos innecesarios para las listas y hacemos un constructor para los usuarios

--- SecondFlow/src/main/java/es/SecondFlow/Usuario.java
+++ SecondFlow/src/main/java/es/SecondFlow/Usuario.java
@@ -4,10 +4,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.SessionScope;
 
 import javax.persistence.*;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
+import java.util.*;
 
 @Entity
 @Component
@@ -18,6 +15,11 @@ public class Usuario {
     @OneToMany(mappedBy = "comprador", cascade = CascadeType.ALL, orphanRemoval = true)
     List<Producto> listaMisProductosComprados;
 
+    @OneToMany(mappedBy = "emisor", cascade = CascadeType.ALL, orphanRemoval = true)
+    List<Conversacion> listaMisConversacionesE;
+
+    @OneToMany(mappedBy = "receptor", cascade = CascadeType.ALL, orphanRemoval = true)
+    List<Conversacion> listaMisConversacionesR;
     String nombreUsuario;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -25,138 +27,18 @@ public class Usuario {
 
 
     public Usuario(String nombreUsuario) {
-        this.listaMisProductos = new List<Producto>() {
-            @Override
-            public int size() {
-                return listaMisProductos.size();
-            }
-
-            @Override
-            public boolean isEmpty() {
-                return listaMisProductos.size() == 0;
-            }
-
-            @Override
-            public boolean contains(Object o) {
-                return false;
-            }
-
-            @Override
-            public Iterator<Producto> iterator() {
-                return null;
-            }
-
-            @Override
-            public Object[] toArray() {
-                return new Object[0];
-            }
-
-            @Override
-            public <T> T[] toArray(T[] a) {
-                return null;
-            }
-
-            @Override
-            public boolean add(Producto producto) {
-                return false;
-            }
-
-            @Override
-            public boolean remove(Object o) {
-                return false;
-            }
-
-            @Override
-            public boolean containsAll(Collection<?> c) {
-                return false;
-            }
-
-            @Override
-            public boolean addAll(Collection<? extends Producto> c) {
-                return false;
-            }
-
-            @Override
-            public boolean addAll(int index, Collection<? extends Producto> c) {
-                return false;
-            }
-
-            @Override
-            public boolean removeAll(Collection<?> c) {
-                return false;
-            }
-
-            @Override
-            public boolean retainAll(Collection<?> c) {
-                return false;
-            }
-
-            @Override
-            public void clear() {
-
-            }
-
-            @Override
-            public boolean equals(Object o) {
-                return false;
-            }
-
-            @Override
-            public int hashCode() {
-                return 0;
-            }
-
-            @Override
-            public Producto get(int index) {
-                return null;
-            }
-
-            @Override
-            public Producto set(int index, Producto element) {
-                return null;
-            }
-
-            @Override
-            public void add(int index, Producto element) {
-
-            }
-
-            @Override
-            public Producto remove(int index) {
-                return null;
-            }
-
-            @Override
-            public int indexOf(Object o) {
-                return 0;
-            }
-
-            @Override
-            public int lastIndexOf(Object o) {
-                return 0;
-            }
-
-            @Override
-            public ListIterator<Producto> listIterator() {
-                return null;
-            }
-
-            @Override
-            public ListIterator<Producto> listIterator(int index) {
-                return null;
-            }
-
-            @Override
-            public List<Producto> subList(int fromIndex, int toIndex) {
-                return null;
-            }
-        };
+        super();
         this.nombreUsuario = nombreUsuario;
+        this.listaMisProductosComprados= new ArrayList<>();
+        this.listaMisConversacionesR = new ArrayList<>();
+        this.listaMisConversacionesE = new ArrayList<>();
+        this.listaMisProductos = new ArrayList<>();
     }
 
     public Usuario() {
 
     }
+
 
 
     public void meterArticuloALista(Producto producto) {
@@ -179,5 +61,14 @@ public class Usuario {
         return this.listaMisProductosComprados;
     }
 
+    public List<Conversacion> getListaMisConversacionesE() {
+        return listaMisConversacionesE;
+    }
+
+    public List<Conversacion> getListaMisConversacionesR() {
+        return listaMisConversacionesR;
+    }
 }
+
+
 
