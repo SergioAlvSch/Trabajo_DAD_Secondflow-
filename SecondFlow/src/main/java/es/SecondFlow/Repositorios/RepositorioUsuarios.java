768b25ca93426205b8524ca4ad4dc3c6bdd732ff
Martín Mochi <78936028+Mvrtn-design@users.noreply.github.com>
2023-03-09 14:29:40 +0100

    actualizando el repositorio usuario

--- SecondFlow/src/main/java/es/SecondFlow/Repositorios/RepositorioUsuarios.java
+++ SecondFlow/src/main/java/es/SecondFlow/Repositorios/RepositorioUsuarios.java
@@ -1,7 +1,10 @@
 package es.SecondFlow.Repositorios;
 
 import es.SecondFlow.Entidades.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaRepository
+
+import java.util.Optional;
 
 public interface RepositorioUsuarios extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByNombreUsuario(String nombreUsurio);
 }
