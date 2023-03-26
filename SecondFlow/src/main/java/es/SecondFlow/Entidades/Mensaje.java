99da45aa6d2abb5bbe96341a9ba845ef2c08cca4
SergioA <123589171+SergioAlvSch@users.noreply.github.com>
2023-03-26 10:45:47 +0200

    Añadimos nuevos atributos a Mensaje

--- SecondFlow/src/main/java/es/SecondFlow/Entidades/Mensaje.java
+++ SecondFlow/src/main/java/es/SecondFlow/Entidades/Mensaje.java
@@ -15,18 +15,36 @@ public class Mensaje {
 
     long idEmisor;
 
+    Boolean usuarioLog;
+
+    String nombreEmisor;
+
 
     @ManyToOne(cascade = CascadeType.ALL)
     Conversacion conversacion;
 
 
-    public Mensaje(String texto, LocalDateTime fecha, long idEmisor,Conversacion conversacion) {
+    public Mensaje(String texto, LocalDateTime fecha, long idEmisor, Conversacion conversacion, String nombreEmisor) {
         super();
+        this.nombreEmisor = nombreEmisor;
         this.texto = texto;
         this.fecha = fecha;
         this.idEmisor = idEmisor;
-        this.conversacion=conversacion;
+        this.conversacion = conversacion;
     }
-    public Mensaje(){
+
+    public Mensaje() {
+    }
+
+    public void setUsuarioLog(Boolean usuarioLog) {
+        this.usuarioLog = usuarioLog;
+    }
+
+    public Boolean getUsuarioLog() {
+        return usuarioLog;
+    }
+
+    public Long getIdEmisor() {
+        return idEmisor;
     }
 }
