99d6137a6549247c55553fa3c48052b6272f7b57
Martín Mochi <78936028+Mvrtn-design@users.noreply.github.com>
2023-04-12 15:32:47 +0200

    Implementacion de cache

--- SecondFlow/src/main/java/es/SecondFlow/Servicios/GestionUsuarios.java
+++ SecondFlow/src/main/java/es/SecondFlow/Servicios/GestionUsuarios.java
@@ -3,43 +3,50 @@ package es.SecondFlow.Servicios;
 import es.SecondFlow.Repositorios.RepositorioUsuarios;
 import es.SecondFlow.Entidades.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@CacheConfig(cacheNames = "usuarios")
 public class GestionUsuarios {
+
     @Autowired
     private RepositorioUsuarios repositorioUsuarios;
 
     public Optional<Usuario> findById(long id) {
         return repositorioUsuarios.findById(id);
     }
-
-    public Usuario findByNombre(String nombre) {
+    @Cacheable
+    public Usuario findByNombre(String nombreUsuario) {
         for (Usuario aux: repositorioUsuarios.findAll()){
-            if (aux.getNombreUsuario().toLowerCase().equals(nombre.toLowerCase())){
+            if (aux.getNombreUsuario().toLowerCase().equals(nombreUsuario.toLowerCase())){
                 return aux;
             }
         }
         return null;
     }
-
+    @CacheEvict(allEntries = true)
+    @Transactional
     public void update(){repositorioUsuarios.flush();}
-
+    @Cacheable
     public boolean exist(long id) {
         return repositorioUsuarios.existsById(id);
     }
-
+    @Cacheable
     public List<Usuario> findAll() {
         return repositorioUsuarios.findAll();
     }
-
+    @CacheEvict(allEntries = true)
     public void save(Usuario usuario) {
         repositorioUsuarios.saveAndFlush(usuario);
     }
-
+    @CacheEvict(allEntries = true)
     public void delete(long id) {
         repositorioUsuarios.deleteById(id);
     }
