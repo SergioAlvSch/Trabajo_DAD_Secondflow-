0ecdee067de35300379a56e8b70cf99c18a62753
SergioA <123589171+SergioAlvSch@users.noreply.github.com>
2023-04-12 15:34:58 +0200

    Update SecondFlowApplication.java

--- SecondFlow/src/main/java/es/SecondFlow/SecondFlowApplication.java
+++ SecondFlow/src/main/java/es/SecondFlow/SecondFlowApplication.java
@@ -2,12 +2,21 @@ package es.SecondFlow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.context.annotation.Bean;
 
+@EnableCaching
 @SpringBootApplication
 public class SecondFlowApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SecondFlowApplication.class, args);
+	}
+	@Bean
+	public CacheManager cacheManager(){
+		return new ConcurrentMapCacheManager("usuarios",/*"productos",*/"conversaciones");
 	}
 
 }
