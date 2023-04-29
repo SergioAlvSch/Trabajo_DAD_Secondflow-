947ae732671cc55b09cc019a4e6dbb74e6f898d1
SergioA <123589171+SergioAlvSch@users.noreply.github.com>
2023-04-29 20:10:06 +0200

    Cambiamos la inicializacion para que solo se realice si la base de datos esta vacia

--- SecondFlow/src/main/java/es/SecondFlow/Servicios/DataBaseInitialitation.java
+++ SecondFlow/src/main/java/es/SecondFlow/Servicios/DataBaseInitialitation.java
@@ -23,22 +23,23 @@ public class DataBaseInitialitation {
 
     @PostConstruct
     public void init(){
+        if(!repositorioUsuarios.findByNombreUsuario("Martin").isPresent()) {
+            Usuario martin = new Usuario("Martin", passwordEncoder.encode("1234"), "martinnosolofutbol@gmail.com", "USER");
+            Usuario sergio = new Usuario("Sergio", passwordEncoder.encode("sas"), "s.alvarezs.2019@alumnos.urjc.es", "USER");
+            Usuario edu = new Usuario("Edu", passwordEncoder.encode("qwert"), "ev.stancu.2019@alumnos.urjc.es", "USER");
 
-        Usuario martin=new Usuario("Martin",passwordEncoder.encode("1234"),"martinnosolofutbol@gmail.com","USER");
-        Usuario sergio = new Usuario("Sergio",passwordEncoder.encode("sas"),"s.alvarezs.2019@alumnos.urjc.es","USER");
-        Usuario edu= new Usuario("Edu",passwordEncoder.encode("qwert"),"ev.stancu.2019@alumnos.urjc.es","USER");
-        repositorioUsuarios.save(martin);
-        repositorioUsuarios.save(sergio);
-        repositorioUsuarios.save(edu);
-        repositorioUsuarios.save(new Usuario("Admin",passwordEncoder.encode("Admin"),"SecondFlowApp@gmail.com","USER","ADMIN"));
+            repositorioUsuarios.save(martin);
+            repositorioUsuarios.save(sergio);
+            repositorioUsuarios.save(edu);
+            repositorioUsuarios.save(new Usuario("Admin", passwordEncoder.encode("Admin"), "SecondFlowApp@gmail.com", "USER", "ADMIN"));
 
-        repositorioProductos.save(new Producto("Balon","Deporte","Der betis",32.05,martin));
-        repositorioProductos.save(new Producto("Sombrero","Deporte","The one piece",1132.05,martin));
-        repositorioProductos.save(new Producto("Televisor","Electronica","Esta roto jeje",320.05,edu));
-        repositorioProductos.save(new Producto("Lampara","Hogar","Es muy acogedora",5,edu));
-        repositorioProductos.save(new Producto("Falda","Deporte","Muy comoda",15,sergio));
-        repositorioProductos.save(new Producto("10 en el examen","Hogar","Es un buen programa, profe creame",32.05,sergio));
-        repositorioProductos.save(new Producto("Zapato","Ropa","Calzado comodo",32.05,martin));
-
+            repositorioProductos.save(new Producto("Balon", "Deporte", "Der betis", 32.05, martin));
+            repositorioProductos.save(new Producto("Sombrero", "Deporte", "The one piece", 1132.05, martin));
+            repositorioProductos.save(new Producto("Televisor", "Electronica", "Esta roto jeje", 320.05, edu));
+            repositorioProductos.save(new Producto("Lampara", "Hogar", "Es muy acogedora", 5, edu));
+            repositorioProductos.save(new Producto("Falda", "Deporte", "Muy comoda", 15, sergio));
+            repositorioProductos.save(new Producto("10 en el examen", "Hogar", "Es un buen programa, profe creame", 32.05, sergio));
+            repositorioProductos.save(new Producto("Zapato", "Ropa", "Calzado comodo", 32.05, martin));
+        }
     }
 }
